1a1716a62ceba89eb5c8cd557ea8448f845ab242
Evelyn <89999600+PokeGon@users.noreply.github.com>
2021-10-07 18:22:37 -0600

    Added a tournament page

--- src/main/views.py
+++ src/main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
+from src.main.models import Tournament
+
 
 def index(request):
     return HttpResponse("Server is up and running.")
+
+
+def tournament(request):
+    currentTournament = Tournament.objects
+    context = {'currentTournament': currentTournament}
+    return render(request, 'tournament', context)

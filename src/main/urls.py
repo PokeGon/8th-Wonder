14478e12f3ae918c857a2acd59100d12ab6e1f07
Evelyn <89999600+PokeGon@users.noreply.github.com>
2021-10-11 11:54:42 -0600

    Added paths to main branch

--- src/main/urls.py
+++ src/main/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('home', views.home, name='home'),
     path('account', views.account, name='account'),
+    path('/accountInfo', views.accountInformation, name='Account Information'),
+    path('/bank', views.bank, name='Bank'),
+    path('/editDrinks', views.drinksEdit, name='Drinks Edit'),
+    path('/orderConfirmation', views.orderConfirmation, name='Order Confirmation'),
+    path('/sponsor', views.sponsor, name='Sponsor'),
+
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -37,3 +37,23 @@ def home(request):
 
 def account(request):
     return render(request,"account.html")
+
+
+def accountInformation(request):
+    return render(request, 'Account Information')
+
+
+def bank(request):
+    return render(request, 'Bank')
+
+
+def drinksEdit(request):
+    return render(request, 'Drinks Edit')
+
+
+def orderConfirmation(request):
+    return render(request, 'Order Confirmation')
+
+
+def sponsor(request):
+    return render(request, 'Sponsor')

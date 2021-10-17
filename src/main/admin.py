85a29185363f40de1a80709907211d8afca9adc2
LordGriswold <yveathan22@gmail.com>
2021-10-16 21:50:27 -0600

    Registered the rest of the models in main/models.py to the main/admin.py page

--- src/main/admin.py
+++ src/main/admin.py
@@ -3,6 +3,15 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Account)
-admin.site.register(BankAccount)
 admin.site.register(User)
+admin.site.register(BankAccount)
 admin.site.register(Transaction)
+admin.site.register(Drink)
+admin.site.register(Manager)
+admin.site.register(Player)
+admin.site.register(Sponsor)
+admin.site.register(Order)
+admin.site.register(Drinkmeister)
+admin.site.register(Tournament)
+admin.site.register(Prize)
+admin.site.register(Score)
